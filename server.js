11119c634c3a57df96d00ebee3b53c38876c503b
PimTournaye <pimtournaye@student.ehb.be>
2022-01-24 12:25:58 +0100

    set up testing express api

--- server.js
+++ server.js
@@ -1,9 +1,32 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3503;
+
+const print = require('./print.js');
+
+const DATE = Date.now().to;
+
+console.log(DATE);
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! \n Printing!')
+  console.log('connected');
+  print.printZebra(`
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  `, 'BIXOLON_BK3_3')
 })
 
 app.listen(port, () => {
