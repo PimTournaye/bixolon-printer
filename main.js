8a144d7c1d73d91f84036330b35205b1c1eccbe3
PimTournaye <pimtournaye@student.ehb.be>
2022-01-31 11:50:08 +0100

    basic loop outlined

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 import Fetcher from './fetch.js';
 import Printer from './printer.js';
-import printer from '@thiagoelg/node-printer';
+import TextFormatter from './textFormatter.js';
 
-let print = new Printer();
+import printer from '@thiagoelg/node-printer';
+import _ from 'lodash';
+
 let fetcher = new Fetcher();
+let formatter = new TextFormatter();
 
 
 /////////////////
@@ -14,6 +17,7 @@ let fetcher = new Fetcher();
 let ticketPrinters = [];
 let printerID = 0;
 
+// Put all Bixolon printers in an array
 printer.getPrinters().forEach(element => {
     let name = element.name
     if (name.includes('BIXOLON')) {
@@ -25,16 +29,44 @@ printer.getPrinters().forEach(element => {
     } else console.log('skipping non-bixolon printer');
 });
 
+let tempPrinterArray = [];
+
+// Make Printer objects for each element in the array
+ticketPrinters.forEach(element => {
+    let printer = new Printer(element.name)
+    tempPrinterArray.push(printer);
+})
+
+// Replace our list of printers with the correct objects
+ticketPrinters = tempPrinterArray;
+
 // Important variables
-const LOOP_TIMER = 10000
+const LOOP_TIMER = 3000
 
 
 ////////////////////
 // MAIN LOOP ///////
 ////////////////////
 
-setInterval(() => {
+setInterval( async () => {
     
+    let message = await fetcher.getLatest()
+
+    if (message.hasNewMessage == true) {
+
+        console.log('new message', message.lastMessage)
+
+        // Get a random printer from our array of available printers
+        let currentPrinter = _.sample(ticketPrinters);
+        //Get rid of HTML tags so we can cleanly print the message
+        let cleanMessage = formatter.stripHTML(message.lastMessage)
+        currentPrinter.printRaw(cleanMessage)
+
+
+    } else {
+        console.log('no new message')
+        console.log(message);
+    }
 }, LOOP_TIMER);
 
 
@@ -45,3 +77,8 @@ setInterval(() => {
 ///////////////////////
 // Other functions ////
 ///////////////////////
+
+// Function for emulating tons of new messages coming in
+const wind = () => {
+
+}
