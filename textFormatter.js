033986a102b261286be92fd857b8a4902c476072
PimTournaye <pimtournaye@student.ehb.be>
2022-01-31 11:50:23 +0100

    stripHTML method

--- textFormatter.js
+++ textFormatter.js
@@ -1,5 +1,10 @@
-class TextFormatter {
+export default class TextFormatter {
     constructor(text){
         this.text = text;
+    }
+
+    stripHTML(text){
+        let strippedString = text.replace(/(<([^>]+)>)/gi, "");
+        return strippedString;
     }
 }
