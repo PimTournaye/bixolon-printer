274d86b406d1cd1918bb8c70c53b8080e227ae3a
PimTournaye <pimtournaye@student.ehb.be>
2022-02-03 15:29:08 +0100

    More commands added to printer

--- printer.js
+++ printer.js
@@ -4,8 +4,10 @@ const PRINTER_NAME = 'BIXOLON_BK3_3'
 
 const INITIAL_PRINTER = '\x1B\x40';
 const PAPER_EJECT = '\x1D\x65\x05';
-
 const PAPER_CUTTING = '\x08\x56\x31';
+
+const IMAGE = '\x1C\x70\x01\x00'
+const FRENCH_CHAR = '\x1B\x52\x01'
 //const PAPER_EJECT = '\29\101\5';
 
 // Will get to refactoring later once I know what I need
@@ -15,10 +17,13 @@ export default class Printer {
     }
 
     printRaw(text){
-        let modifiedText = '\n \n' + text + "\n \n \n \n" 
-        
+        console.log(typeof text);
+        let modifiedText = "\n \n" + text + "\n \n \n \n" 
+        console.log(typeof Buffer.from(modifiedText), Buffer.from(modifiedText));
+        console.log(typeof this.name, this.name);
         printDirect({
-            data: Buffer.from(INITIAL_PRINTER) + Buffer.from(modifiedText) + Buffer.from(PAPER_CUTTING) + Buffer.from(PAPER_EJECT) , 
+            //data: Buffer.from(INITIAL_PRINTER).toString() + Buffer.from(modifiedText).toString() + Buffer.from(PAPER_CUTTING).toString() + Buffer.from(PAPER_EJECT).toString(), 
+            data: Buffer.from(INITIAL_PRINTER) + Buffer.from(IMAGE) + Buffer.from(FRENCH_CHAR) + Buffer.from(modifiedText) + Buffer.from(PAPER_CUTTING) + Buffer.from(PAPER_EJECT), 
             printer:this.name,
             type: "RAW",
             success:function(){	console.log("printed: \n"+text + '\n \n');},
