9c5bfaaca62ccd9607069073fe50ecd5128c1b5e
PimTournaye <pim.tournaye@student.ehb.be>
2022-02-04 10:40:14 +0000

    small refactor plus testing string

--- printer.js
+++ printer.js
@@ -1,4 +1,7 @@
 import { printDirect } from '@thiagoelg/node-printer';
+import legacy from "legacy-encoding";
+import _ from 'lodash';
+
 const PRINTER_NAME = 'BIXOLON_BK3_3'
 
 
@@ -9,6 +12,7 @@ const PAPER_CUTTING = '\x08\x56\x31';
 const IMAGE = '\x1C\x70\x01\x00'
 const FRENCH_CHAR = '\x1B\x52\x01'
 
+let testString = ` ' character test << >> … ÀÁÂÏȊ äæéẽëěęḙḛéêèçöṻ c'est « test » & @ ‼ ⁈ '`
 // Will get to refactoring later once I know what I need
 export default class Printer {
     constructor(printerName){
@@ -18,14 +22,12 @@ export default class Printer {
     printRaw(text){
         console.log(typeof text);
         let modifiedText = "\n \n" + text + "\n \n \n \n" 
-        console.log(typeof Buffer.from(modifiedText), Buffer.from(modifiedText));
-        console.log(typeof this.name, this.name);
+        _.unescape(modifiedText)
         printDirect({
-            //data: Buffer.from(INITIAL_PRINTER).toString() + Buffer.from(modifiedText).toString() + Buffer.from(PAPER_CUTTING).toString() + Buffer.from(PAPER_EJECT).toString(), 
-            data: Buffer.from(INITIAL_PRINTER) + Buffer.from(IMAGE) + Buffer.from(FRENCH_CHAR) + Buffer.from(modifiedText) + Buffer.from(PAPER_CUTTING) + Buffer.from(PAPER_EJECT), 
+            data: this.prepareData(testString), 
             printer:this.name,
             type: "RAW",
-            success:function(){	console.log("printed: \n"+text + '\n \n');},
+            success:function(){	},
             error:function(err){console.log(err);}
         });
     }
@@ -48,18 +50,9 @@ export default class Printer {
         });
     }
 
-    print(data){
-        printDirect({
-            data: Buffer.from(INITIAL_PRINTER) + Buffer.from(data) + Buffer.from(PAPER_CUTTING) + Buffer.from(PAPER_EJECT) , 
-            printer:this.name,
-            type: "RAW",
-            success:function(){	console.log("printed: \n"+text + '\n \n');},
-            error:function(err){console.log(err);}
-        });
-    }
-
     prepareData(data){
-
+        let preparedData = Buffer.from(INITIAL_PRINTER) + Buffer.from(IMAGE) + Buffer.from(FRENCH_CHAR) + Buffer.from(data, 'utf8') + Buffer.from(PAPER_CUTTING) + Buffer.from(PAPER_EJECT);
+        return preparedData;
     }
 
     applySpecialOptions(){
